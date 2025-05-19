6258f0d62a3bf9485f4fb8780b5330527684b522
Priyadharshinisundharam <dharshinisundharam2001@gmail.com>
2025-05-19 21:45:50 +0530

    SQL file added

---
+++ TelcoCustomer SQL.sql
@@ -0,0 +1,5 @@
+SELECT * FROM telcocustomer.telco_cleaned;
+
+SELECT * FROM telcocustomer.`telco_cleaned` where Gender = 'Male'
+
+SELECT * FROM telcocustomer.`telco_cleaned` WHERE Gender = 'Female'
