17f6e076a8057f152186f03de9e5ba8ae9df8e75
Priyadharshinisundharam <dharshinisundharam2001@gmail.com>
2025-05-19 21:54:14 +0530

    Delete TelcoCustomer SQL.sql

--- TelcoCustomer SQL.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM telcocustomer.telco_cleaned;
-
-SELECT * FROM telcocustomer.`telco_cleaned` where Gender = 'Male'
-
-SELECT * FROM telcocustomer.`telco_cleaned` WHERE Gender = 'Female'
