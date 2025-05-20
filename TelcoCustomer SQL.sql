05d6c5fb8da2f89e9e6476c7ed1718aa5c47763f
Priyadharshinisundharam <dharshinisundharam2001@gmail.com>
2025-05-20 13:38:32 +0530

    file modified

--- TelcoCustomer SQL.sql
+++ TelcoCustomer SQL.sql
@@ -1,5 +1,17 @@
 SELECT * FROM telcocustomer.telco_cleaned;
 
-SELECT * FROM telcocustomer.`telco_cleaned` where Gender = 'Male'
+select * FROM telcocustomer.`telco_cleaned` WHERE Gender = "Male"
 
-SELECT * FROM telcocustomer.`telco_cleaned` WHERE Gender = 'Female'
+SELECT * FROM telcocustomer.`telco_cleaned` WHERE Gender = "Female"
+
+SELECT * FROM telcocustomer.`telco_cleaned` WHERE Senior_Citizen = "Yes"
+
+SELECT * FROM telcocustomer.`telco_cleaned` WHERE Senior_Citizen = "No"
+
+# the count of male customers
+SELECT COUNT('Gender') FROM telcocustomer.`telco_cleaned` WHERE Gender = "Male"
+
+# the count of female customers
+SELECT COUNT('Gender') FROM telcocustomer.`telco_cleaned` WHERE Gender = "Female"
+
+SELECT COUNT(*) from telcocustomer.`telco_cleaned`
